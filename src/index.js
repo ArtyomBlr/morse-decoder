f490385902341072b3347d7fd0ac51391df855ee
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-03-20 17:08:11 +0300

    feat: add function, but it is incorrect

--- src/index.js
+++ src/index.js
@@ -37,74 +37,28 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-/*const MORSE_TABLE_NUMBER = {
-    '0000001011': 'a',
-    '0011101010': 'b',
-    '0011101110': 'c',
-    '0000111010': 'd',
-    '0000000010': 'e',
-    '0010101110': 'f',
-    '0000111110': 'g',
-    '0010101010': 'h',
-    '0000001010': 'i',
-    '0010111111': 'j',
-    '0000111011': 'k',
-    '0010111010': 'l',
-    '0000001111': 'm',
-    '0000001110': 'n',
-    '0000111111': 'o',
-    '0010111110': 'p',
-    '0011111011': 'q',
-    '0000101110': 'r',
-    '0000101010': 's',
-    '0000000011': 't',
-    '0000101011': 'u',
-    '0010101011': 'v',
-    '0000101111': 'w',
-    '0011101011': 'x',
-    '0011101111': 'y',
-    '0011111010': 'z',
-    '1011111111': '1',
-    '1010111111': '2',
-    '1010101111': '3',
-    '1010101011': '4',
-    '1010101010': '5',
-    '1110101010': '6',
-    '1111101010': '7',
-    '1111111010': '8',
-    '1111111110': '9',
-    '1111111111': '0',
-};*/
-
 function decode(expr) {
-    let decodedMorse = '',
-    decodedMorseWords = '',
-    letters,
-    firstEl = 0;
+    const PATTERN = /........../g;
+    const numberTen = /10/g;
+    const numberEleven = /11/g;
+    const numberZero = /00/g;
+    const space = /\*........./g;
+ 
+    let arr = expr.match(PATTERN);
+    
+    for(let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].replace(numberZero, "");
+        arr[i] = arr[i].replace(numberEleven, ".");
+        arr[i] = arr[i].replace(numberTen, "-");
+        arr[i] = arr[i].replace(space, " ");
 
-   for(let i = 0; i < expr.length/10; i++) {
-       
-       letters = expr.slice(firstEl, firstEl + 10);
-       firstEl += 10;
-
-       if(letters == '**********'){
-           decodedMorse += " ";
-       }
-
-        for(let j = 0; j <= 10; j += 2){
-            switch(letters.slice(j, j + 2)) {
-                case '10': decodedMorseWords += '.'; 
-                break;
-                case '11': decodedMorseWords += '-'; 
-                break;
-                default: break;
-            }
-        }
-        decodedMorse += MORSE_TABLE[decodedMorseWords];
+        arr[i] = MORSE_TABLE[arr[i]];
     }
-   return decodedMorse;
-}
 
+    return arr.join("");
+
+
+}
 module.exports = {
     decode
 }
